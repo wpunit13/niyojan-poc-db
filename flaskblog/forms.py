e5ea4ca193fea2edbce451a65d3345c5a7848108
wpunit13 <wpunit13@gmail.com>
2020-02-22 13:16:04 +0530

    Password Reset

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,76 +1,59 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed, FileField
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
+
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(),
-                                                   Length(min=2, max=20)])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-                        validators=[
-                            DataRequired(),
-                            Email()
-                        ])
-    password = PasswordField('Password',
-                             validators=[DataRequired()]
-                             )
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                validators=[DataRequired(),
-                                         EqualTo('password')])
+                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username = username.data).first()
+        user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is already in use, please use different username')
+            raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email = email.data).first()
+        user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is already in use, please use different email')
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
-                        validators=[
-                            DataRequired(),
-                            Email()
-                        ])
-    password = PasswordField('Password',
-                             validators=[DataRequired()]
-                             )
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
-class UpdateAccount(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(),
-                                                   Length(min=2, max=20)])
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-                        validators=[
-                            DataRequired(),
-                            Email()
-                        ])
-    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('That username is already in use, please use different username')
-       # else:
-        #    raise ValidationError('Please enter different email from your current email.')
-
+                raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is already in use, please use different email')
-       # else:
-        #    raise ValidationError('Please enter different email from your current email.')
+                raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class PostForm(FlaskForm):
@@ -81,23 +64,17 @@ class PostForm(FlaskForm):
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email',
-                        validators=[
-                            DataRequired(),
-                            Email()
-                        ])
-    submit = SubmitField('Update')
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email = email.data).first()
+        user = User.query.filter_by(email=email.data).first()
         if user is None:
-            raise ValidationError('There is no account registered in this app with this email')
+            raise ValidationError('There is no account with that email. You must register first.')
 
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password',
-                             validators=[DataRequired()]
-                             )
+    password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(),
-                                                 EqualTo('password')])
+                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
